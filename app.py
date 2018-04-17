83272c37ff223f44f068f5a6bb57b531224caea4
iamshooock <36152272+iamshooock@users.noreply.github.com>
2018-04-17 13:31:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ import random
 
 guessesTaken = 0
 
-number = random.randint(1, 1000)
-print('Well , I am thinking of a number between 1 and 1000.')
+number = random.randint(1, 20)
+print('Well , I am thinking of a number between 1 and 20.')
 
 while guessesTaken < 6:
      print('Take a guess.') # There are four spaces in front of print.
