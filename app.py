bdece6dfc1c17dc4e9ab9272cff09c29638e80a7
iamshooock <36152272+iamshooock@users.noreply.github.com>
2018-04-17 12:37:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -94,32 +94,6 @@ def apple_news():
             link = head + data['href']
         content += '{}\n\n'.format(link)
     return content
-
-
-import random
-
-print('Hello! What is your name?')
-name = input()
-
-print('Well! I am thinking a number between 1-20.')
-secretNumber= random.randint(1,20)
-
-for guesstimes in range(1,7):
-    print('Put your guess number :')
-    guessNum = int(input())
-
-    if guessNum>secretNumber:
-        print('The value is too high.\n')
-    elif guessNum<secretNumber:
-        print('The value is too low.\n')
-    else:
-        break   # This break when gress is correct value.
-
-
-if guessNum == secretNumber:
-    print('Good Job, '+name+'. You got correct number in '+str(guesstimes)+' guess!')
-else:
-    print('Nope! Game Over. The gress value is '+str(secretNumber)+'.')
     
 
 def get_page_number(content):
